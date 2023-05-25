9bfa99f2fee3549e9c298474471bf3600f51e4d0
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-25 15:47:25 -0300

    feat: added yup validation in cities with translations for errors

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,27 +1,37 @@
 import { Request, Response } from 'express'
 import * as yup from 'yup'
+import StatusCodes from 'http-status-codes'
 
 interface ICidade {
   nome: string
+  estado: string
 }
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 })
 
 
 export const create = async (req: Request<{},{},ICidade>,res: Response ) => {
   let validatedDate: ICidade | undefined = undefined
   try{
-    validatedDate = await bodyValidation.validate(req.body)
-  } catch(error) {
-    const yupError = error as yup.ValidationError
+    validatedDate = await bodyValidation.validate(req.body, { abortEarly: false })
+  } catch(err) {
 
-    return res.json({
-      errors: {
-        default: yupError.message
-      }
+    const yupError = err as yup.ValidationError
+    const errors: Record<string, string>= {}
+
+    yupError.inner.forEach(error => {
+
+      if(!error.path) return
+      
+
+      errors[error.path] = error.message
     })
+
+
+    return res.status(StatusCodes.BAD_REQUEST).json({errors})
   }
   console.log(validatedDate)
 
