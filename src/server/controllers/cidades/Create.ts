9085d55792a4f72577f636a768961af0e5df7b5f
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-25 15:16:22 -0300

    feat: creation schemas for validations

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,16 +1,29 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 
 interface ICidade {
   nome: string
 }
 
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+})
 
-export const create = (req: Request<{},{},ICidade>,res: Response ) => {
 
-  const data: ICidade = req.body
+export const create = async (req: Request<{},{},ICidade>,res: Response ) => {
+  let validatedDate: ICidade | undefined = undefined
+  try{
+    validatedDate = await bodyValidation.validate(req.body)
+  } catch(error) {
+    const yupError = error as yup.ValidationError
 
-  console.log(data.nome)
-
+    return res.json({
+      errors: {
+        default: yupError.message
+      }
+    })
+  }
+  console.log(validatedDate)
 
   return res.send('Create!')
 }
