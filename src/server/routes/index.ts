2ea618a186a0c547ba0dc21dfb563bbd2c038fdd
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-26 11:20:28 -0300

    feat: added routes to methods

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,7 +9,10 @@ router.get('/',(rez,res) => {
 
 
 router.get('/cidades',CidadesController.getAllValidation,CidadesController.getAll)
+router.get('/cidades/:id',CidadesController.getByIdValidation,CidadesController.getById)
 router.post('/cidades',CidadesController.createValidation,CidadesController.create)
+router.put('/cidades/:id',CidadesController.updateByIdValidation,CidadesController.updateById)
+router.delete('/cidades/:id',CidadesController.deleteByIdValidation, CidadesController.deleteById)
 
 
 
