f8639cb9aa7ac2b48b1f3582ef3197f15f1a10eb
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-26 11:17:05 -0300

    feat: added method getById

---
+++ src/server/controllers/cidades/GetById.ts
@@ -0,0 +1,20 @@
+import * as yup from 'yup'
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+
+import { validation } from '../../shared/middleware'
+interface IParamProps {
+  id?: number
+}
+
+export const getByIdValidation =  validation(getSchema => ({
+  params: getSchema<IParamProps>(yup.object().shape({
+    id: yup.number().integer().required().moreThan(0)
+  })),
+}))
+
+export const getById =  async (req: Request<IParamProps>,res: Response) => {
+  console.log(req.params)
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!')
+}
