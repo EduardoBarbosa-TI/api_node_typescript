cd641497fcae2ae412fcac753a67798c8cccee00
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-26 11:17:17 -0300

    feat: added method updateById

---
+++ src/server/controllers/cidades/UpdateById.ts
@@ -0,0 +1,29 @@
+import * as yup from 'yup'
+
+import { validation } from '../../shared/middleware'
+import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
+
+interface iParamsProps{
+  id?: number
+}
+
+interface IBodyProps{
+  nome: string
+}
+
+export const updateByIdValidation = validation(getSchema => ({
+  body: getSchema<IBodyProps>(yup.object().shape({
+    nome: yup.string().required().min(3)
+  })),
+  params: getSchema<iParamsProps>(yup.object().shape({
+    id: yup.number().integer().required().moreThan(0)
+  }))
+}))
+
+export const updateById =  async (req: Request<iParamsProps,{},IBodyProps>,res: Response) => {
+  console.log(req.params)
+  console.log(req.body)
+
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!')
+}
