070b772837213c225f826397839d97a9355574e6
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-29 18:15:23 -0300

    fix: string restriction change and interface change

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,20 +1,27 @@
+import { CidadesProvider } from '../../database/providers/cidades'
 import { Request, Response } from 'express'
-import * as yup from 'yup'
+import { ICidade } from '../../database/models'
 import { validation } from '../../shared/middleware'
+import * as yup from 'yup'
 import { StatusCodes } from 'http-status-codes'
 
-interface ICidade {
-  nome: string
-}
+interface IBodyProps extends Omit<ICidade, 'id'> {}
 
 export const createValidation =  validation((getSchema) =>({
-  body: getSchema<ICidade>(yup.object().shape({
-    nome: yup.string().required().min(3)
+  body: getSchema<IBodyProps>(yup.object().shape({
+    nome: yup.string().required().min(3).max(150),
   }))
 }))
 
 export const create = async (req: Request<{},{},ICidade>,res: Response ) => {
-  console.log(req.body)
+  const result = await CidadesProvider.create(req.body)
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    })
+  }
 
-  return res.status(StatusCodes.CREATED).json(1)
+  return res.status(StatusCodes.CREATED).json(result)
 }
