7189f67c03ad26603833882bf9656660b2840a4b
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-05-29 22:39:36 -0300

    feat: added cidades updateById provider with knex

---
+++ src/server/database/providers/cidades/UpdateById.ts
@@ -0,0 +1,18 @@
+import { ETableNames } from '../../ETableNames'
+import { ICidade } from '../../models'
+import { Knex } from '../../knex'
+
+export const updateById =  async (id: number, cidade: Omit<ICidade, 'id'>): Promise<void | Error> => {
+  try {
+    const result = await Knex(ETableNames.cidade)
+      .update(cidade)
+      .where('id', '=', id)
+      
+    if (result > 0) return 
+    
+    return new Error('Registro não encontrado')
+  } catch (error) {
+    console.log(error)
+    return new Error('Registro não encontrado')
+  }
+}
